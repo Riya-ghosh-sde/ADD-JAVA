d215d8a5a30f9409bbfb767963ce7424ae5fcb48
Riya Ghosh <riaghosh419@gmail.com>
2023-12-16 11:44:59 +0530

    Add 3 no in java

---
+++ Add3no.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AddThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        int sum = num1 + num2 + num3;
+
+        System.out.println("The sum of " + num1 + ", " + num2 + ", and " + num3 + " is: " + sum);
+
+        scanner.close();
+    }
+}
